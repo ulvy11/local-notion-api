626bd53560f464d09c6da296fc64b80a7a188320
ulvy11 <152696191+ulvy11@users.noreply.github.com>
2024-03-16 16:26:18 +0100

    feat(express): router and backend

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { Config } from "./config/config";
+import { notionRouter } from "./notion/notion-router";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = Number.parseInt(Config.getProperty(Config.PORT));
+
+app.get("/api/hello", (_: Request, res: Response) => {
+  res.send("Hello lady ♥<3!");
+});
+
+app.use("/notion", notionRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
